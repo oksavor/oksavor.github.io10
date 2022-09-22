b5da700e486480067808339155399982bcbfe47f
savor <103667951+oksavor@users.noreply.github.com>
2022-09-22 10:43:05 -0500

    Add files via upload

---
+++ home-cdn/typewriter-custom.js
@@ -0,0 +1,7 @@
+const instance = new Typewriter('#typewriter', {
+  strings: ['the best', 'unstopble', 'cheap', 'amazing', 'perfect', 'insane', 'jojo reference'],
+  autoStart: true,
+  loop: true,
+});
+
+
